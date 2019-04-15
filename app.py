fe610b7ccb38e2959048fedc0fca5980ae706ab4
Ebonne Cabarrus <cabarrus.ebonne@gmail.com>
2019-04-15 15:22:32 -0700

    added second implementation of random words back in

--- app.py
+++ app.py
@@ -26,17 +26,17 @@ def tweet_gen():
     return HTML.format(random_sentence)
 
 #implementation where user can generate as many words as they want
-# @app.route('/words/<int:num>')
-# def tweet_gen(num):
-#     histo_text = get_words('siddhartha.txt')
-#     clean_text = get_tokens(histo_text)
-#     histo = histogram(clean_text)
-#     random_word = sample(histo)
-#     random_words = []
-#     for i in range(num):
-#         random_words.append(sample(histo))
-#     random_sentence = sentence_maker(random_words)
-#     return HTML.format(random_sentence)
+@app.route('/words/<int:num>')
+def tweet_gen(num):
+    histo_text = get_words('siddhartha.txt')
+    clean_text = get_tokens(histo_text)
+    histo = histogram(clean_text)
+    random_word = sample(histo)
+    random_words = []
+    for i in range(num):
+        random_words.append(sample(histo))
+    random_sentence = sentence_maker(random_words)
+    return HTML.format(random_sentence)
 
 
 if __name__ == '__main__':
